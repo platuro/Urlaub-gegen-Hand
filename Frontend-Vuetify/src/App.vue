1bf029cfb07291a6f9458efe9d69b00621f2b22c
Platuro (Daniel) <30547642+platuro@users.noreply.github.com>
2025-12-29 16:27:02 +0100

    revert footer changes

--- Frontend-Vuetify/src/App.vue
+++ Frontend-Vuetify/src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <v-app class="app-root">
-    <div class="layout">
-      <v-main class="content">
-        <router-view />
-      </v-main>
+      <router-view />
       <Footer />
-    </div>
   </v-app>
 </template>
 
@@ -15,20 +11,5 @@ export default { components: { Footer } };
 </script>
 
 <style>
-html, body, #app { height: 100%; margin: 0; }
 
-.app-root {
-  min-height: 100vh;
-}
-
-.layout {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-.content {
-  flex: 1;
-  padding-bottom: 0 !important;
-}
 </style>
